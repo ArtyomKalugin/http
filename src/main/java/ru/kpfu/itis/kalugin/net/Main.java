49b43fdd4031c71bf2682d36b40c2d5598d9dcc2
Artem <Artem>
2021-09-11 16:30:57 +0300

    Initial commit

---
+++ src/main/java/ru/kpfu/itis/kalugin/net/Main.java
@@ -0,0 +1,21 @@
+package ru.kpfu.itis.kalugin.net;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String, String> headers = new HashMap<>();
+        Map<String, String> params = new HashMap<>();
+
+        headers.put("Content-Type", "application/json");
+        headers.put("Accept", "application/json");
+        params.put("userId", "1");
+        params.put("photo", "none");
+
+        System.out.println(new HttpClientRealisation().get("https://postman-echo.com/get",
+                headers, params));
+        System.out.println(new HttpClientRealisation().post("https://postman-echo.com/post",
+                headers, params));
+    }
+}
